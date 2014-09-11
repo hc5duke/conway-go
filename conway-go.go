3fab8141ddcd501f08fa798cb40758a46a885a85
Hwan-Joon Choi <hc5duke@gmail.com>
2014-09-11 00:41:58 -0700

    learning how to use multi-dim slices

--- conway-go.go
+++ conway-go.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+var (
+	rows  = 20
+	cols  = 20
+	board = MakeBoard(rows, cols)
+)
+
+func MakeBoard(rows, cols int) [][]bool {
+	_board := make([][]bool, rows)
+	for i := range _board {
+		_board[i] = make([]bool, cols) /* again the type? */
+		for j := range _board[i] {
+			_board[i][j] = false
+		}
+	}
+	return _board
+}
 func main() {
 	fmt.Println("xxx")
+	fmt.Println(board)
 }
